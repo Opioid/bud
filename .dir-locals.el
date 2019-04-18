154ed663ebd2d60d63831e58eb62d609551aba19
Opioid <benjamin.savs@gmail.com>
2019-04-18 15:59:40 +0200

    Set counsel-etags backend for rust

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
 ((nil . ((projectile-project-compilation-cmd . "cmake --build build --config Release --target p2studio")
 		 (projectile-project-run-cmd . "cd system && cargo run")
-		 )))
+		 (counsel-etags-update-tags-backend . (lambda (src-dir) (shell-command "rusty-tags emacs"))))))
