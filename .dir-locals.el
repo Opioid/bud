2bdeff766b202b96e077468b08dfb1446aded963
Opioid <benjamin.savs@gmail.com>
2019-05-07 15:54:04 +0200

    cargo check mapped to F7 now

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((projectile-project-compilation-cmd . "cmake --build build --config Release --target p2studio")
+((nil . ((projectile-project-compilation-cmd . "cargo check")
 		 (projectile-project-run-cmd . "./build_and_run.sh")
 		 (counsel-etags-update-tags-backend . (lambda (src-dir) (shell-command "rusty-tags emacs"))))))
